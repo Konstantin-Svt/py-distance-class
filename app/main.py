cb1ae56e1aa4af45eed41cde6b6470b080b0e43f
Konstantin S. <kostaho16@gmail.com>
2025-07-22 19:35:42 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
 from __future__ import annotations
-from typing import Any
 
 
 class Distance:
@@ -12,52 +11,63 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: Any) -> Distance:
+    def __add__(self, other: int | float | Distance) -> Distance:
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
         elif isinstance(other, int) or isinstance(other, float):
             return Distance(self.km + other)
+        raise TypeError("Expected Distance, int, or float type")
 
-    def __iadd__(self, other: Any) -> Distance:
+    def __iadd__(self, other: int | float | Distance) -> Distance:
         if isinstance(other, Distance):
             self.km += other.km
             return self
         elif isinstance(other, int) or isinstance(other, float):
             self.km += other
             return self
+        raise TypeError("Expected Distance, int, or float type")
 
-    def __mul__(self, other: Any) -> Distance:
-        return Distance(self.km * other)
+    def __mul__(self, other: int | float) -> Distance:
+        if isinstance(other, int) or isinstance(other, float):
+            return Distance(self.km * other)
+        raise TypeError("Expected int or float type")
 
-    def __truediv__(self, other: Any) -> Distance:
-        return Distance(round((self.km / other), 2))
+    def __truediv__(self, other: int | float) -> Distance:
+        if isinstance(other, int) or isinstance(other, float):
+            return Distance(round((self.km / other), 2))
+        raise TypeError("Expected int or float type")
 
-    def __lt__(self, other: Any) -> bool:
+    def __lt__(self, other: int | float | Distance) -> bool:
         if isinstance(other, Distance):
             return self.km < other.km
-        elif isinstance(other, int) or isinstance(other, float):
+        if isinstance(other, int) or isinstance(other, float):
             return self.km < other
+        raise TypeError("Expected Distance, int, or float type")
 
-    def __gt__(self, other: Any) -> bool:
+    def __gt__(self, other: int | float | Distance) -> bool:
         if isinstance(other, Distance):
             return self.km > other.km
-        elif isinstance(other, int) or isinstance(other, float):
+        if isinstance(other, int) or isinstance(other, float):
             return self.km > other
+        raise TypeError("Expected Distance, int, or float type")
 
-    def __eq__(self, other: Any) -> bool:
+    def __eq__(self, other: int | float | Distance) -> bool:
         if isinstance(other, Distance):
             return self.km == other.km
-        elif isinstance(other, int) or isinstance(other, float):
+        if isinstance(other, int) or isinstance(other, float):
             return self.km == other
+        raise TypeError("Expected Distance, int, or float type")
 
-    def __le__(self, other: Any) -> bool:
+    def __le__(self, other: int | float | Distance) -> bool:
         if isinstance(other, Distance):
             return self.km <= other.km
-        elif isinstance(other, int) or isinstance(other, float):
+        if isinstance(other, int) or isinstance(other, float):
             return self.km <= other
+        raise TypeError("Expected Distance, int, or float type")
 
-    def __ge__(self, other: Any) -> bool:
+    def __ge__(self, other: int | float | Distance) -> bool:
         if isinstance(other, Distance):
             return self.km >= other.km
-        elif isinstance(other, int) or isinstance(other, float):
+        if isinstance(other, int) or isinstance(other, float):
             return self.km >= other
+        raise TypeError("Expected Distance, int, or float type")
